c015a21c3bc731825564d9dee62475688516b1f7
Darrylwin <logossoudarryl20@gmail.com>
2026-01-25 11:03:24 +0000

    allowed all origins

--- essivivi/settings.py
+++ essivivi/settings.py
@@ -185,6 +185,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'drf_yasg',
+    'corsheaders',
 
     # Local apps
     'authentication',
@@ -201,6 +202,7 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'authentication.Admin'
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -327,3 +329,30 @@ SWAGGER_SETTINGS = {
 # Configuration Email (pour le développement)
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEFAULT_FROM_EMAIL = 'noreply@essivivi.com'
+
+# Configuration CORS - AUTORISE TOUTES LES ORIGINES
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+
+# En-têtes autorisés
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+# Méthodes HTTP autorisées
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
