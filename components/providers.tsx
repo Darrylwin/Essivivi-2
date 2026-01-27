d7ba9166db5b68bd3d81edee952c9a11f5473e94
Darrylwin <logossoudarryl20@gmail.com>
2026-01-27 09:04:29 +0000

    updated default app theme to light

--- components/providers.tsx
+++ components/providers.tsx
@@ -11,7 +11,7 @@ export function Providers({ children }: ProvidersProps) {
   return (
     <ThemeProvider
       attribute="class"
-      defaultTheme="dark"
+      defaultTheme="light"
       enableSystem
       disableTransitionOnChange
     >
